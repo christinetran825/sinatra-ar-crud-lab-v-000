a1204d657a47f7337161d5db0b2bef1d3c433e34
Christine Tran <christinetran825@gmail.com>
2017-10-18 17:58:43 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,29 +11,29 @@ class ApplicationController < Sinatra::Base
   get '/posts/new' do
     erb :new
   end
-  
+
   post '/posts' do
     @post = Post.create(name: params[:name], content: params[:content])
     redirect to "/posts"
   end
-  
+
   #READ
   get '/posts' do
     @posts = Post.all
     erb :index
   end
-  
+
   get '/posts/:id' do
     @post = Post.find_by_id(params[:id])
     erb :show
   end
-  
+
   #UPDATE
   get '/posts/:id/edit' do
     @post = Post.find_by_id(params[:id])
     erb :edit
   end
-  
+
   patch '/posts/:id' do
     @post = Post.find_by_id(params[:id])
     @post.name = params[:name]
@@ -41,7 +41,7 @@ class ApplicationController < Sinatra::Base
     @post.save
     redirect to "/posts/#{@post.id}"
   end
-  
+
   #DELETE
   delete '/posts/:id/delete' do
     @post = Post.find_by_id(params[:id])
