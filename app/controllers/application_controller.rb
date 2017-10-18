27ee8cb8f0652cac38714e5620fa5f709bcc2a97
Christine Tran <christinetran825@gmail.com>
2017-10-18 17:46:25 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,7 +33,7 @@ class ApplicationController < Sinatra::Base
     @post = Post.find_by_id(params[:id])
     erb :edit
   end
-  
+
   patch '/posts/:id' do
     @post = Post.find_by_id(params[:id])
     @post.name = params[:name]
