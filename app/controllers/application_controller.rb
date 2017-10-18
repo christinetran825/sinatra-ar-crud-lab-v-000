c2650a1fed850105790f90b1dfdc1342beac0822
Christine Tran <christinetran825@gmail.com>
2017-10-18 17:46:38 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -42,10 +42,10 @@ class ApplicationController < Sinatra::Base
     redirect to "/posts/#{@post.id}"
   end
 
-  #DELETE
-  # delete '/posts/:id/delete' do
-  #   @post = Post.find_by_id(params[:id])
-  #   @post.delete
-  #   erb :delete
-  # end
+  DELETE
+  delete '/posts/:id/delete' do
+    @post = Post.find_by_id(params[:id])
+    @post.delete
+    erb :delete
+  end
 end
